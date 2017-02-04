005cfe69cedf7bda136a426e9a5efe520d16d2f1
Berezkin Aleksandr <tepmeh90694@yandex.ru>
2017-02-04 21:36:11 +0300

    tabs whitout adaptive design

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,1 +1,10 @@
 // tabs js me
+$(document).ready(function(){
+    $('input').click(function () {
+    	var tab_id = $(this).attr('data-tabs');
+
+    	$('.content').removeClass('active');
+
+    	$('#'+tab_id).addClass('active');
+    })
+});
